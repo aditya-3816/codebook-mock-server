45d67e9c296369a835ca4ce1c92cd5e3cf80ae62
aditya-3816 <apatil7@deloitte.com>
2024-03-18 11:38:14 +0530

    CodeBook Backend

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
-import jsonServer from "json-server";
 import auth from "json-server-auth";
+import jsonServer from "json-server";
+
 
 const server = express();
 server.use((req, res, next) => {
